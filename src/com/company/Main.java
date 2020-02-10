c46136b8d784e8109471cb568878216ef84654ce
mansurx2009 <itcom.0219@gmail.com>
2020-02-10 18:40:09 -0500

    jojo1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String name = "Hello";
-        System.out.println(name);
+        String namea = "Hello";
+        System.out.println(namea);
+        String [] names = {"Mansur","Akbar","Aziz","Jurabek"};
+        int [] nameS1 = {10,20,30,40};
+
+        for (String name : names){
+            System.out.println(name);
+
+            for (int i : nameS1){
+                System.out.println(i);
+            }
+        }
+
     }
 }
